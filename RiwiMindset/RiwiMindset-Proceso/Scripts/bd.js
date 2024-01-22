3509ed0fc6b04e1103239638ff5d759bad1f1451
Jhamile08 <111102978+Jhamile08@users.noreply.github.com>
2024-01-21 21:26:05 -0500

    Add files via upload

---
+++ RiwiMindset/RiwiMindset-Proceso/Scripts/bd.js
@@ -0,0 +1,124 @@
+// export let estudiantes = [
+//   {
+//   id: 1,
+//   "nombre": "lily",
+//   "cedula": 123,
+//   "correo": "lili",
+//   "admin": true,
+//   "contrasena": 333
+// },
+// {
+//   "id": "",
+//   "nombre": "Jhamile Andrea Dominguez Rendon",
+//   "cedula": 2,
+//   "correo": "",
+//   "admin": false,
+//   "recomendaciones": [
+//     {
+      
+//     }
+//   ],
+//   "observaciones": [
+//     {
+      
+//     }
+//   ],
+//   "contrasena": 2
+// },
+// {
+//   "id": "",
+//   "nombre": "Daniel Parra",
+//   "cedula": 100636133,
+//   "correo": "",
+//   "recomendaciones": [
+//     {
+      
+//     }
+//   ],
+//   "observaciones": [
+//     {
+      
+//     }
+//   ],
+//   "contrasena": ""
+// },
+// {
+//   "id": "",
+//   "nombre": "e",
+//   "cedula": 1040571136,
+//   "correo": "",
+//   "recomendaciones": [
+//     {
+      
+//     }
+//   ],
+//   "observaciones": [
+//     {
+
+//     }
+//   ],
+//   "contrasena": ""
+// }
+// ];
+// Por ahora solo con objeto
+  
+
+
+
+
+export let estudiantes = [
+  {
+   id: 1,
+   nombre: "Andres Ospina",
+   cedula: 1,
+   correo: "",
+   admin: true,
+   contrasena: 1,
+  },
+  {
+    id: 2,
+    nombre: "lily",
+    cedula: 2,
+    correo: "",
+    admin: true,
+    contrasena: 2
+   },
+   {
+    id: 3,
+    nombre: "Samantha Agudelo",
+    cedula: 3,
+    correo: "",
+    admin: false,
+    contrasena: 3
+   },
+   {
+    id: 4,
+    nombre: "Martin Ramirez",
+    cedula: 4,
+    correo: "",
+    admin: false,
+    contrasena: 4
+   },
+   {
+    id: 5,
+    nombre: "Luisa Fernanda Lopez",
+    cedula: 5,
+    correo: "",
+    admin: false,
+    contrasena: 5
+   },
+   {
+    id: 6,
+    nombre: "Jhohan Stiven Parra",
+    cedula: 6,
+    correo: "",
+    admin: false,
+    contrasena: 6
+   }
+
+
+
+]
+
+
+
