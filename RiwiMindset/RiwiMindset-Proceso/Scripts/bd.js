983392a68f184970fca60d54e973e7c0d5c9131d
TradeDanielP <tradedanielp@gmail.com>
2024-01-22 07:44:35 -0500

    se agrego propiedad foto a la base de datos

--- RiwiMindset/RiwiMindset-Proceso/Scripts/bd.js
+++ RiwiMindset/RiwiMindset-Proceso/Scripts/bd.js
@@ -60,64 +60,72 @@
 //   "contrasena": ""
 // }
 // ];
+
+
+
+// <script src="../Scripts/indexTeachersUsers.js"></script>
+// import { estudiantes } from "../Scripts/bd.js";
 // Por ahora solo con objeto
   
 
 
 
 
-export let estudiantes = [
+let estudiantes = [
   {
-   id: 1,
-   nombre: "Andres Ospina",
-   cedula: 1,
-   correo: "",
-   admin: true,
-   contrasena: 1,
+    id: 1,
+    foto: "../Images/fondoLogin1.png",
+    nombre: "Andres Ospina",
+    cedula: 1,
+    correo: "",
+    admin: true,
+    contrasena: 1,
   },
   {
     id: 2,
+    foto: "../Images/logoImagen.png",
     nombre: "lily",
     cedula: 2,
     correo: "",
     admin: true,
     contrasena: 2
-   },
-   {
+  },
+  {
     id: 3,
+    foto: "../Images/logoImagen.png",
     nombre: "Samantha Agudelo",
     cedula: 3,
     correo: "",
     admin: false,
     contrasena: 3
-   },
-   {
+  },
+  {
     id: 4,
+    foto: "../Images/logoImagen.png",
     nombre: "Martin Ramirez",
     cedula: 4,
     correo: "",
     admin: false,
     contrasena: 4
-   },
-   {
+  },
+  {
     id: 5,
+    foto: "../Images/logoImagen.png",
     nombre: "Luisa Fernanda Lopez",
     cedula: 5,
     correo: "",
     admin: false,
     contrasena: 5
-   },
-   {
+  },
+  {
     id: 6,
+    foto: "../Images/logoImagen.png",
     nombre: "Jhohan Stiven Parra",
     cedula: 6,
     correo: "",
     admin: false,
     contrasena: 6
-   }
-
-
-
+  }
 ]
 
 
