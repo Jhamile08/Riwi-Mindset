904d98efbf0011380d8208e9034c91ca8129e5ad
areviRPC <csebastian.riverap@gmail.com>
2024-01-29 13:44:34 -0500

    validar que las citas solo puedan ser reservadas de la fecha actual en adelante, ordenar citas cronologicamente del home, insertar dia de la semana en la card de cita en home

--- RiwiMindset/RiwiMindset-Proceso/Scripts/TeachersSchedule.js
+++ RiwiMindset/RiwiMindset-Proceso/Scripts/TeachersSchedule.js
@@ -69,27 +69,36 @@ document.addEventListener("DOMContentLoaded", function () {
     const eventName = document.getElementById("eventName").value;
     const eventDate = document.getElementById("eventDate").value;
     const eventTime = document.getElementById("eventTime").value;
+    const reason = document.getElementById("reason").value;
     /* Formatea la hora para que coincida con el formato esperado en el calendar (añadiendo ":00" al final) */
     const formattedTime = `${eventTime}:00`;
-
+    // Formatea la fecha y hora para comparación
+    const currentDateTime = moment();
+    const selectedDateTime = moment(`${eventDate} ${eventTime}`,"YYYY-MM-DD HH:mm");
+    // Verificar si la fecha seleccionada es anterior a la fecha y hora actuales
+    if (selectedDateTime.isBefore(currentDateTime)) {
+      alert("No puedes reservar citas en fechas anteriores a la actual.");
+      return;}
     // Verificar si el horario está disponible - empezamos con un bloque try-catch para manejar posibles errores durante la ejecución del código
     try {
       // verificamos si el horario seleccionado no esta ocupado segun la funcion isTimeSlotOccupied
-      if (!await isTimeSlotOccupied(calendar, `${eventDate}T${formattedTime}`)) {
+      if (
+        !(await isTimeSlotOccupied(calendar, `${eventDate}T${formattedTime}`))
+      ) {
         // creamos el evento con propiedades especificas del calendar
-          const newEvent = {
+        const newEvent = {
           title: eventName,
           start: moment(`${eventDate}T${formattedTime}`).format(),
-          end: moment(`${eventDate}T${formattedTime}`).add(1, 'hour').format(),
-          reason: 'Ansiedad',
+          end: moment(`${eventDate}T${formattedTime}`).add(1, "hour").format(),
+          reason: reason,
           date: eventDate,
-          time: eventTime
+          time: eventTime,
         };
         // Enviar el nuevo evento al servidor usando fetch con una solicitud POST al servidor JSON.
-        const response = await fetch('http://localhost:4002/events', {
-          method: 'POST',
+        const response = await fetch("http://localhost:4002/events", {
+          method: "POST",
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
           body: JSON.stringify(newEvent),
         });
@@ -101,15 +110,17 @@ document.addEventListener("DOMContentLoaded", function () {
           eventForm.reset();
           // Captura cualquier error que pueda ocurrir durante la ejecución del bloque try y muestra un mensaje de error.
         } else {
-          console.error('Error al agregar evento:', response.status);
-          alert('Hubo un error al agregar el evento. Por favor, intenta de nuevo.');
+          console.error("Error al agregar evento:", response.status);
+          alert(
+            "Hubo un error al agregar el evento. Por favor, intenta de nuevo."
+          );
         }
       } else {
         alert("¡Este horario ya está reservado! Por favor, elige otro.");
       }
     } catch (error) {
-      console.error('Error al agregar evento:', error);
-      alert('Hubo un error al agregar el evento. Por favor, intenta de nuevo.');
+      console.error("Error al agregar evento:", error);
+      alert("Hubo un error al agregar el evento. Por favor, intenta de nuevo.");
     }
   });
 
