60b0ae5762fc32f6199913adf60a71cbdf1b9883
areviRPC <patoasesino69@gmail.com>
2024-01-29 14:10:52 -0500

    trabajndo en pdf

--- RiwiMindset/RiwiMindset-Proceso/Scripts/Users-registrer.js
+++ RiwiMindset/RiwiMindset-Proceso/Scripts/Users-registrer.js
@@ -5,9 +5,7 @@
 const datos = JSON.parse(localStorage.getItem("gente"))
 
 
-
-
-function inyect(){
+export function inyect(){
 
     /* inyectar foto y card */
     const user = document.querySelector('#data-user');
@@ -16,21 +14,38 @@ function inyect(){
     coderUser.innerHTML= `
         <img src="${datos.foto}" alt="" id="foto";></img>
         <p id="nombre">${datos.nombre}</p>
-        <p id="nombre">  edad: ${datos.edad}</p>
+        <p id="nombre">  <b> edad: </b> ${datos.edad}</p>
         
     `;
     user.appendChild(coderUser);
 
-    /* inyectar fecha */
 
-    const agenda = document.querySelector('.fechaEntrevista');
-    agenda.textContent = datos.fecha[0]
-
-
-    /* inyectar observaciones */
-
-    const recomendaciones = document.querySelector('.registroInfo');
-    recomendaciones.textContent = datos.recomendaciones[0]
 }
 
 inyect(datos)
+
+
+
+console.log(datos.fecha.length);
+
+for (var i=0 ; i < datos.fecha.length; i++){
+    
+    const historial = document.querySelector("#registro")
+    
+    const regsitro = document.createElement('div')
+    regsitro.innerHTML = `
+    <div class="registroEspecifico">
+    <p class="fechaEntrevista">
+    <b> fecha: </b> ${datos.fecha[i]}
+    </p>
+    <p class="registroInfo">
+        ${datos.recomendaciones[i]}
+    </p>
+    <button>Editar</button>
+    <button>Ver más</button>
+    `
+    historial.appendChild(regsitro)
+
+    console.log("nada");
+}
+
