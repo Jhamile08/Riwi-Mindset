b74074d82126a777bd5680beb64e7dd8238db449
jhamile08 <caf9vasquez@gmail.com>
2024-01-30 15:20:01 -0500

    2

--- RiwiMindset/RiwiMindset-Proceso/Teachers/Scripts/Users-registrer.js
+++ RiwiMindset/RiwiMindset-Proceso/Teachers/Scripts/Users-registrer.js
@@ -1,36 +1,46 @@
-
-
-
-
 const datos = JSON.parse(localStorage.getItem("gente"))
-console.log(datos);
 
 
-
-function inyect(){
+export function inyect(){
 
     /* inyectar foto y card */
     const user = document.querySelector('#data-user');
 
     const coderUser = document.createElement('div')
     coderUser.innerHTML= `
-        <img src="${datos[5]}" alt="" id="foto";></img>
-        <p id="nombre">${datos[0]}</p>
-        <p id="nombre">  edad: ${datos[1]}</p>
+        <img src="${datos.foto}" alt="" id="foto";></img>
+        <p id="nombre">${datos.nombre}</p>
+        <p id="nombre">  <b> edad: </b> ${datos.edad}</p>
         
     `;
     user.appendChild(coderUser);
 
-    /* inyectar fecha */
 
-    const agenda = document.querySelector('.fechaEntrevista');
-    agenda.textContent = datos[2]
-
-
-    /* inyectar observaciones */
-
-    const recomendaciones = document.querySelector('.registroInfo');
-    recomendaciones.textContent = datos[4]
 }
 
 inyect(datos)
+
+
+
+console.log(datos.fecha.length);
+
+for (var i=0 ; i < datos.fecha.length; i++){
+    
+    const historial = document.querySelector("#registro")
+    
+    const regsitro = document.createElement('div')
+    regsitro.innerHTML = `
+    <div class="registroEspecifico">
+    <p class="fechaEntrevista">
+    <b> fecha: </b> ${datos.fecha[i]}
+    </p>
+    <p class="registroInfo">
+        ${datos.recomendaciones[i]}
+    </p>
+    <button>Editar</button>
+    <button>Ver más</button>
+    `
+    historial.appendChild(regsitro)
+
+    console.log("nada");
+}
