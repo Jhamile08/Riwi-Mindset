f4a0ea4d67094351e8c19bec74e1115313a92ade
jhamile08 <jhamiledr222@gmail.com>
2024-01-31 10:36:38 -0500

    Reestructuracion del proyecto

--- RiwiMindset/RiwiMindset-Proceso/Teachers/Scripts/test.js
+++
@@ -1,9 +0,0 @@
-/* inyeccion foto y nombre */
-
-import { inyect } from "./Users-registrer.js"
-
-document.addEventListener('DOMContentLoaded',inyect(datos))
-
-const datos = JSON.parse(localStorage.getItem("gente"))
-
-console.log(datos);
