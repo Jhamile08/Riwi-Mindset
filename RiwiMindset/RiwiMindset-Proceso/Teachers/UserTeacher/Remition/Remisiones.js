b8c9870d51b665ccc4564bec30ae2ba0a2ef77a2
areviRPC <csebastian.riverap@gmail.com>
2024-02-01 09:30:27 -0500

    buscador dinamico con su css

--- RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/Remition/Remisiones.js
+++ RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/Remition/Remisiones.js
@@ -1,102 +1,117 @@
+import { enfermedades } from "./enfermedades.js";
+const datos = JSON.parse(localStorage.getItem("gente"));
 
-import { enfermedades } from "./enfermedades.js"
-const datos = JSON.parse(localStorage.getItem("gente"))
+/* inyectar foto y card */
+function inyect() {
+  const user = document.querySelector("#data-user");
 
-    /* inyectar foto y card */
-function inyect(){
-
-
-    const user = document.querySelector('#data-user');
-
-    const coderUser = document.createElement('div')
-    coderUser.innerHTML= `
+  const coderUser = document.createElement("div");
+  coderUser.innerHTML = `
         <img src="${datos.foto}" alt="" id="foto";></img>
         <p id="nombre">${datos.nombre}</p>
         <p id="nombre">  edad: ${datos.edad}</p>
         
     `;
-    user.appendChild(coderUser);
-    
+  user.appendChild(coderUser);
 }
 
-/* Inyeccion del PDF */ 
+/* Inyeccion del PDF */
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Escuchamos el click del botón
-    const $boton = document.querySelector("#btnCrearPdf");
-    $boton.addEventListener("click", () => {
-        const $elementoParaConvertir = document.querySelector('#PDF'); // <-- Aquí puedes elegir cualquier elemento del DOM
-        var text = document.getElementById('PDF');
-        // Ajusta el tamaño del textarea al contenido
-        text.style.height = text.scrollHeight + "px";
-        
-        html2pdf()
-            .set({
-                margin: 0.5,
-                filename: `${datos.nombre}.pdf`,
-                image: {
-                    type: 'jpeg',
-                    quality: 0.98
-                },
-                html2canvas: {
-                    scale: 3, // A mayor escala, mejores gráficos, pero más peso
-                    letterRendering: true,
-                },
-                jsPDF: {
-                    unit: "in",
-                    format: "a3",
-                    orientation: 'portrait' // landscape o portrait
+  // Escuchamos el click del botón
+  const $boton = document.querySelector("#btnCrearPdf");
+  $boton.addEventListener("click", () => {
+    const $elementoParaConvertir = document.querySelector("#PDF"); // <-- Aquí puedes elegir cualquier elemento del DOM
+    var text = document.getElementById("PDF");
+    // Ajusta el tamaño del textarea al contenido
+    text.style.height = text.scrollHeight + "px";
 
-                }
-            })
+    html2pdf()
+      .set({
+        margin: 0.5,
+        filename: `${datos.nombre}.pdf`,
+        image: {
+          type: "jpeg",
+          quality: 0.98,
+        },
+        html2canvas: {
+          scale: 3, // A mayor escala, mejores gráficos, pero más peso
+          letterRendering: true,
+        },
+        jsPDF: {
+          unit: "in",
+          format: "a3",
+          orientation: "portrait", // landscape o portrait
+        },
+      })
 
-            .from($elementoParaConvertir)
-            .save()
-            .catch(err => console.log(err));
-            
-
-    });
+      .from($elementoParaConvertir)
+      .save()
+      .catch((err) => console.log(err));
+  });
 });
 
+/* inyeccion buscador en el pdf */
 
-/* inyeccion buscador */
+document.addEventListener("DOMContentLoaded", () => {
+  inyect(datos);
+  injectionBuscador(enfermedades);
 
+  const motivo = document.querySelectorAll(".codigo");
 
-document.addEventListener("DOMContentLoaded",() =>{
-        inyect(datos);
-        injectionBuscador(enfermedades);
-        
-        
-        const motivo = document.querySelectorAll(".codigo")
+  motivo.forEach((coder) => {
+    coder.addEventListener("click", (e) => {
+      e.preventDefault();
+      let consulta = document.querySelector("#consulta");
 
-        motivo.forEach((coder) =>{
-            coder.addEventListener('click',(e)=>{
-                e.preventDefault()
-                let consulta = document.querySelector('#consulta')
-                
-                consulta.value = e.target.text 
-            })
-        })
-    
-})
+      consulta.value = e.target.text;
+    });
+  });
+});
 
-function injectionBuscador(codigos){
-    let mostrar = document.querySelector("#cont-enfermedades");
-    
-    codigos.forEach((enfermedades)=>{
-        const {nombre,codigo,especifico} = enfermedades
-        /* destructuring */
-        const aEnfermedad = document.createElement("p");
-        aEnfermedad.innerHTML= `
+function injectionBuscador(codigos) {
+  let mostrar = document.querySelector("#cont-enfermedades");
+
+  codigos.forEach((enfermedades) => {
+    const { nombre, codigo, especifico } = enfermedades;
+    /* destructuring */
+    const aEnfermedad = document.createElement("p");
+    aEnfermedad.innerHTML = `
         <div class="dms">
-        <a width: 30%; href="" class="codigo"> > ${codigo} - ${nombre} (${especifico})  </a>
+        <a width: 30%; href="" class="codigo"> > ${codigo} - ${nombre} (${especifico})  </a> <hr>
         </div>
         
         `;
-        mostrar.appendChild(aEnfermedad);  
-        
-    })}
+    mostrar.appendChild(aEnfermedad);
+  });
+}
 
+/* buscador de enfermedades (usa ccs) */
 
+// evento input para guardar el nombre a buscar
 
+const patologia = document.querySelector("#patologia");
 
+patologia.addEventListener("input", (e) => {
+  let patologia = e.target.value;
+  console.log(patologia);
+  nombre(patologia);
+});
+
+// funcion para buscar coders (se usa CSS)
+
+function nombre(patologia) {
+  const name = document.querySelectorAll(".codigo");
+  console.log(name);
+  name.forEach((e) => {
+    if (
+      e.textContent.toLocaleLowerCase().includes(patologia.toLocaleLowerCase())
+    ) {
+      console.log("coincide");
+      e.parentElement.classList.remove("busqueda");
+    } else {
+      console.log("no coincide");
+      e.parentNode.classList.add("busqueda");
+    }
+  });
+}
--- RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/indexTeachersUsers.js
+++ RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/indexTeachersUsers.js
@@ -1,24 +1,23 @@
-import { estudiantes } from "/General/Data/bd.js"
+import { estudiantes } from "/General/Data/bd.js";
 
-// inyeccion de los estudiantes 
+// inyeccion de los estudiantes
 
-estudiantes.forEach((estudiantes)=>{
-    console.log(estudiantes);
-})
+estudiantes.forEach((estudiantes) => {
+  console.log(estudiantes);
+});
 
 let arreglo = [];
 injectionCoderHtml(estudiantes);
 
+function injectionCoderHtml(coders) {
+  let inyeccionCoders = document.querySelector("#container-users");
 
-
-function injectionCoderHtml(coders){
-    let inyeccionCoders = document.querySelector("#container-users");
-    
-    coders.forEach((estudiantes)=>{
-        const {id,foto, nombre,edad,recomendaciones,observaciones,fecha} = estudiantes
-        /* destructuring */
-        const coderHtml = document.createElement("p");
-        coderHtml.innerHTML= `
+  coders.forEach((estudiantes) => {
+    const { id, foto, nombre, edad, recomendaciones, observaciones, fecha } =
+      estudiantes;
+    /* destructuring */
+    const coderHtml = document.createElement("p");
+    coderHtml.innerHTML = `
         <div class="estudents">
             <p id="nombre">${nombre}</p>
             <a href="/Teachers/UserTeacher/Registrer/IndexTeachersUsers-register.html" type="submit" id ="${id}" class= "actualizar">Actualizar</a>
@@ -27,58 +26,58 @@ function injectionCoderHtml(coders){
         </div>
 
         `;
-        inyeccionCoders.appendChild(coderHtml);
-        const data = {
-            id,
-            foto,
-            nombre,
-            edad,
-            recomendaciones,
-            observaciones,
-            fecha}
+    inyeccionCoders.appendChild(coderHtml);
+    const data = {
+      id,
+      foto,
+      nombre,
+      edad,
+      recomendaciones,
+      observaciones,
+      fecha,
+    };
 
-            arreglo.push(data);
+    arreglo.push(data);
 
-        // Capturar con el queryselector
+    // Capturar con el queryselector
+  });
 
-    })
-    
-    console.log(arreglo);
-    
-    const actualizar = document.querySelectorAll('.actualizar');
-    
-    actualizar.forEach((persona)=>{
-        persona.addEventListener('click',(e)=>{
-            localStorage.setItem("gente",JSON.stringify(arreglo[Number(e.target.getAttribute("id")) - 1]))  
+  console.log(arreglo);
 
-        }) 
-    })
+  const actualizar = document.querySelectorAll(".actualizar");
 
+  actualizar.forEach((persona) => {
+    persona.addEventListener("click", (e) => {
+      localStorage.setItem(
+        "gente",
+        JSON.stringify(arreglo[Number(e.target.getAttribute("id")) - 1])
+      );
+    });
+  });
 
-// evento input para guardar el nombre a buscar 
+  // evento input para guardar el nombre a buscar
 
-document.addEventListener('input',(e)=>{
-    let apellido = e.target.value
+  document.addEventListener("input", (e) => {
+    let apellido = e.target.value;
     console.log(apellido);
     nombre(apellido);
-})
+  });
 
-// funcion para buscar coders (se usa CSS)
+  // funcion para buscar coders (se usa CSS)
 
-function nombre(apellido){
-    const name = document.querySelectorAll("#nombre")
+  function nombre(apellido) {
+    const name = document.querySelectorAll("#nombre");
     console.log(name);
-    name.forEach(e =>{
-        if(e.textContent.toLocaleLowerCase().includes(apellido.toLocaleLowerCase())){
-            console.log("coincide")
-            e.parentElement.classList.remove('buscador')
-        }else{
-            console.log("no coincide");
-            e.parentNode.classList.add('buscador')
-        }
-})}
-};
-
-/* Captura de datos y envio al local storage */
-
-
+    name.forEach((e) => {
+      if (
+        e.textContent.toLocaleLowerCase().includes(apellido.toLocaleLowerCase())
+      ) {
+        console.log("coincide");
+        e.parentElement.classList.remove("buscador");
+      } else {
+        console.log("no coincide");
+        e.parentNode.classList.add("buscador");
+      }
+    });
+  }
+}
