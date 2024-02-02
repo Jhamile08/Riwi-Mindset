edcb4cfa3eb440f989e01b8e409adc012a1ea0f7
TradeDanielP <tradedanielp@gmail.com>
2024-02-02 11:30:19 -0500

    Cambiar la rama main por la developer

--- RiwiMindset/RiwiMindset-Proceso/General/Data/bd.js
+++ RiwiMindset/RiwiMindset-Proceso/General/Data/bd.js
@@ -1,55 +1,73 @@
 export let estudiantes = [
   {
   "id": 1,
-  "edad": 20,
   "nombre": "lily",
-  "foto": "../Images/fondoLogin1.png",
+  "foto": "/General/Images/ImagesTeachers/fondoLogin1.png",
   "cedula": 123,
   "correo": "lili",
   "admin": true,
-  "fecha" : ["20/30/2023","drtdtr"],
-  "recomendaciones": ["sadasda","asdasda"],
-  "observaciones": ["mmmmmm Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, debitis explicabo. Dolor porro sapiente repudiandae eligendi non nostrum ad aliquid, ratione dignissimos expedita eos reiciendis, inventore illum","asdkaposdkaopsdkaopdssoapdk"],
-  "contrasena": 333,
+  "contrasena": 333
 },
 {
   "id": "",
-  "edad": 20,
   "nombre": "Jhamile Andrea Dominguez Rendon",
-  "foto": "../Images/fondoLogin1.png",
+  "foto": "/General/Images/ImagesTeachers/fondoLogin1.png",
   "cedula": 2,
   "correo": "",
-  "admin": true,
-  "fecha" : ["asdadadas"],
-  "recomendaciones": ["sadasda"],
-  "observaciones": ["adasdada"],
-  "contrasena": 333,
+  "admin": false,
+  "recomendaciones": [
+    {
+      
+    }
+  ],
+  "observaciones": [
+    {
+      
+    }
+  ],
+  "contrasena": 2
 },
 {
   "id": "",
-  "edad": 20,
   "nombre": "Daniel Parra",
-  "foto": "../Images/fondoLogin1.png",
+  "foto": "/General/Images/ImagesTeachers/fondoLogin1.png",
   "cedula": 100636133,
   "correo": "",
-  "admin": true,
-  "fecha" : ["asdadadas"],
-  "recomendaciones": ["sadasda"],
-  "observaciones": ["adasdada"],
-  "contrasena": 333,
+  "recomendaciones": [
+    {
+      
+    }
+  ],
+  "observaciones": [
+    {
+      
+    }
+  ],
+  "contrasena": ""
 },
 {
   "id": "",
-  "edad": 20,
   "nombre": "e",
-  "foto": "../Images/fondoLogin1.png",
+  "foto": "/General/Images/ImagesTeachers/fondoLogin1.png",
   "cedula": 1040571136,
   "correo": "",
-  "admin": true,
-  "fecha" : ["asdadadas"],
-  "recomendaciones": ["sadasda"],
-  "observaciones": ["adasdada"],
-  "contrasena": 333,
+  "recomendaciones": [
+    {
+      "fecha" : 1,
+      "textoRecomendaciones" : "a"
+    },
+    {
+      "fecha" : 1,
+      "textoRecomendaciones" : "a"
+    }
+    
+  ],
+  "observaciones": [
+    {
+
+    }
+  ],
+  "contrasena": ""
 }
 ];
 
