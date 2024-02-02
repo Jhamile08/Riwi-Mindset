0022142cc76d6538815d3fb5c903afa501e9fde1
areviRPC <csebastian.riverap@gmail.com>
2024-02-02 13:34:17 -0500

    integracion de user.registers de franco en mi rama

--- RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/Registrer/Users-registrer.js
+++ RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/Registrer/Users-registrer.js
@@ -1,17 +1,18 @@
 const datos = JSON.parse(localStorage.getItem("gente"))
 
 
+
+
 function inyect(){
 
     /* inyectar foto y card */
     const user = document.querySelector('#data-user');
 
     const coderUser = document.createElement('div')
-    coderUser.innerHTML= `<div class= perfil >
-    <img src="${datos.foto}" alt="" id="foto";></img> <br>
-    <p id="nombre">${datos.nombre}</p> <br>
-    <p id="nombre">  edad: ${datos.edad}</p>
-  </div>
+    coderUser.innerHTML= `
+        <img src="${datos.foto}" alt="" id="foto";></img>
+        <p id="nombre">${datos.nombre}</p>
+        <p id="nombre">  edad: ${datos.edad}</p>
         
     `;
     user.appendChild(coderUser);
@@ -38,14 +39,88 @@ for (var i=0 ; i < datos.fecha.length; i++){
     <p class="registroInfo">
         ${datos.recomendaciones[i]}
     </p>
-    <button class="btnVerMas" onclick="mostrarVerMas(${[i]})">Ver más</button>
+    <button id="btnfuera btn btn1" class="btnVerMas" onclick="mostrarVerMas(${[i]})" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Ver más</button>
+
+    <button id="btnfuera btn btn1" class="btnEditar" onclick="mostrarEditar(${[i]})" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Editar</button>
     `
     historial.appendChild(regsitro);
     
-    console.log("nada");
     function mostrarVerMas(i){
-        const parrafo = document.getElementById('textarea');
+        /* const parrafo = document.getElementById('textarea');
         parrafo.innerText = datos.observaciones[i];
-        document.getElementById("textarea").style.display = "block";
+        document.getElementById("textarea").style.display = "block"; */
+
+        /* FUNCION QUE DETECTA Y MUESTRA LAS O/R SEGÚN EL ESTUDIANTE */
+        const btn1 = document.querySelector('.btn1');
+        btn1.innerText = "Ver Recomendaciones";
+
+        const btn2 = document.querySelector('.btn2');
+        btn2.innerText = "Ver Observaciones";
+
+        const parrafoObservaciones = document.querySelector('.pObservaciones');
+        parrafoObservaciones.innerHTML= `
+        ${datos.observaciones[i]}
+        <br>
+        ${datos.fecha[i]}
+        `
+        const parrafoRecomendaciones = document.querySelector('.pRecomendaciones');
+        parrafoRecomendaciones.innerHTML= `
+        ${datos.recomendaciones[i]}
+        <br>
+        ${datos.fecha[i]}
+        `
+        
+        /* document.getElementById("exampleModalToggle1").style.display = "block";
+        document.getElementById("exampleModalToggle2").style.display = "none";
+        
+        mostrarRecomendaciones(i) */
+        
+        
+        
     }
+
+    function mostrarEditar(i){
+
+        const btn1 = document.querySelector('.btn1');
+        btn1.innerText = "Ver Recomendaciones";
+
+        const btn2 = document.querySelector('.btn2');
+        btn2.innerText = "Ver Observaciones";
+
+        const btn3 = document.querySelector('.btn3');
+        btn3.innerText = "Guardar edición";
+
+        const btn4 = document.querySelector('.btn4');
+        btn4.innerText = "Guardar edición";
+
+        
+        const parrafoObservaciones = document.querySelector('.pObservaciones');
+        parrafoObservaciones.innerHTML= `
+        <textarea style="width: 100%" id="textAreaObservaciones" oninput="${guardarObservaciones()}">${datos.observaciones[i]}</textarea>
+        `
+        const parrafoRecomendaciones = document.querySelector('.pRecomendaciones');
+        parrafoRecomendaciones.innerHTML= `
+        <textarea style="width: 100%" id="textAreaRecomendaciones" oninput="${guardarRecomendaciones()}">${datos.recomendaciones[i]}</textarea>
+        `
+
+        function guardarObservaciones(){
+            const textAreaObservaciones = document.querySelector('#textAreaObservaciones');
+            
+            datos.observaciones[i].replace(`${datos.observaciones[i]}`,`${textAreaObservaciones}`)
+            console.log(datos.observaciones[i]);
+        }
+
+        function guardarRecomendaciones(){
+            const textAreaRecomendaciones = document.querySelector('#textAreaRecomendaciones');
+            
+            datos.recomendaciones[i].replace(`${datos.observaciones[i]}`,`${textAreaRecomendaciones}`)
+            console.log(datos.recomendaciones[i]);
+        }
+
+                
+    }
+    /* function mostrarRecomendaciones(i){
+        document.getElementById("exampleModalToggle2").style.display = "block";
+        document.getElementById("exampleModalToggle1").style.display = "none";
+    } */
 }
