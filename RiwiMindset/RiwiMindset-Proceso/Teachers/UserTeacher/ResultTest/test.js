eb7a09aed0905319e096b4331bb66cb27b758c6d
cfranco-11 <caf9vasquez@gmail.com>
2024-02-07 07:58:04 -0500

    inyeccion card y foto test

--- RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/ResultTest/test.js
+++ RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/ResultTest/test.js
@@ -1,4 +1,6 @@
 
+const datos = JSON.parse(localStorage.getItem("gente"));
+
 let tabla = document.querySelector('.slide tbody')
 
 tabla.innerHTML += `
@@ -8,3 +10,24 @@ tabla.innerHTML += `
     <td>10%</td>
     </tr>
 `
+
+
+/* inyectar foto y card */
+
+function inyect() {
+    const user = document.querySelector("#dataUser");
+
+    const coderUser = document.createElement("div");
+    coderUser.innerHTML = `<div class= perfil >
+        <img src="${datos.foto}" alt="" id="foto";></img> <br>
+        <p id="nombre">${datos.nombre}</p> <br>
+        <p id="nombre">  edad: ${datos.edad}</p>
+    </div>
+            
+        `;
+    user.appendChild(coderUser);
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+    inyect(datos);
+})
