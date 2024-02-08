1b5a291aa9a03bd2f3523cd84f6fb1d5ce4a4fcb
Camiloriwi <camilomanco2005@gmail.com>
2024-02-08 13:35:25 -0500

    actualizacion exitosa del server

--- RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/Registrer/Users-registrer.js
+++ RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/Registrer/Users-registrer.js
@@ -1,157 +1,126 @@
+//obtenemos los datos de localstorage
+const datos = JSON.parse(localStorage.getItem("gente"))
 
-//Generamos una url base
-const urlBase = "http://localhost:4002/";
-// Seleccionamos el contenedor principal donde vamos a inyectars los coders
-let inyeccionCoders = document.querySelector("#container-users");
-//Funcion que obtiene los datos desde el archivo json
-async function getStudent() {
-    const response = await fetch(`${urlBase}students`);
-    const data = response.json();
-    return data;
-};
+function inyect() {
 
-
-async function inyect() {
-    const students = await getStudent();
     /* inyectar foto y card */
     const user = document.querySelector('#data-user');
+
     const coderUser = document.createElement('div')
-    
-    students.forEach(student => {
-        coderUser.innerHTML= `
-            <img src="${student.foto}" alt="" id="foto";></img>
-            <p id="nombre">${student.nombre}</p>
-            <p id="nombre">  edad: ${student.edad}</p>
-            
-        `;
-    })
+    coderUser.innerHTML = `
+<img src="${datos.foto}" alt="" id="foto";></img>
+<p id="nombre">${datos.nombre}</p>
+<p id="nombre"> edad: ${datos.edad}</p>
+`;
     user.appendChild(coderUser);
 
     /* inyectar fecha */
 
-
 }
 
-inyect();
+inyect(datos);
 
+console.log(datos.fecha.length);
 
-
-async function inyectHistoria() {
-    const students = await getStudent();
+function inyectHistoria() {
     const historial = document.querySelector("#registro")
-    
-    const register = document.createElement('div')
-    students.forEach(student => {
-        register.innerHTML = `
-        <div class="registroEspecifico">
-        <p class="fechaEntrevista">
-        <b> fecha: </b> ${student.fecha}
-        </p>
-        <p class="registroInfo">
-            ${student.recomendaciones}
-        </p>
-        <button id="btnfuera btn" class="btnVerMas" onclick="mostrarVerMas()" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Ver más</button>
-        `
-        
-    })
+    const regsitro = document.createElement('div')
+    regsitro.innerHTML = `
+<div class="registroEspecifico">
+<p class="fechaEntrevista">
+<b> fecha: </b> ${datos.fecha[i]}
+</p>
+<p class="registroInfo">
+${datos.recomendaciones[i]}
+</p>
+<button id="btnfuera btn" class="btnVerMas" onclick="mostrarVerMas(${[i]})" data-bs-toggle="modal" href="#exampleModalToggle" role="button">Ver más</button>
+`
     /* CAMBIO DE CLASE TN 2 */
-    /* EL BOTON 2  */
-    historial.appendChild(register);
-}
-improve()
-async function improve() {
-    const students = await getStudent();
-    students.forEach(student => {
-
-            inyectHistoria()
-
-            /* MODAL VER MAS */
-            async function mostrarVerMas(i) {
-                const students = await getStudent();
-
-                const btn1 = document.querySelector('.btn1');
-                btn1.innerText = "VER RECOMENDACIONES"; /* BOTONES DENTRO DEL MODAL */
-
-                const btn3 = document.querySelector('.btn3');
-                btn3.innerText = "BOTON 3";
-
-                const btn2 = document.querySelector('.btn2');
-                btn2.innerText = "VER OBSERVACIONES";
-
-                const btn4 = document.querySelector('.btn4');
-                btn4.innerText = "BOTON 4";
-
-                /* evento click para las funciones de editar */
-
-                const parrafoObservaciones = document.querySelector('.pObservaciones');
-                parrafoObservaciones.innerHTML = `
-        ${student.observaciones}
-        <br>
-        ${student.fecha}
-        `
-                const parrafoRecomendaciones = document.querySelector('.pRecomendaciones');
-                parrafoRecomendaciones.innerHTML = `
-        ${student.recomendaciones}
-        <br>
-        ${student.fecha}
-        `
-                btn3.addEventListener('click', mostrarEditar(i));
-                btn4.addEventListener('click', mostrarEditar(i));
-
-            }
-
-            function mostrarEditar(i) {
-
-                console.log("funcion editar ");
-                const parrafoObservaciones = document.querySelector('.pObservaciones');
-                parrafoObservaciones.innerHTML = `
-        <textarea style="width: 100%" id="textAreaObservaciones" oninput='${edicionObs()}'>${student.observaciones}</textarea>
-        <br>
-        <p> fecha: ${student.fecha}</p>
-        <br>
-        `
-                const parrafoRecomendaciones = document.querySelector('.pRecomendaciones');
-                parrafoRecomendaciones.innerHTML = `
-        <textarea style="width: 100%" id="textAreaRecomendaciones" oninput='${edicionReco()}'>${student.recomendaciones}</textarea>
-        <br>
-        <p>fecha: ${student.fecha}</p>
-        <br>
-        `
-            }
-        
-    });
-   
+    /* EL BOTON 2 */
+    historial.appendChild(regsitro);
 }
 
+for (var i = 0; i < datos.fecha.length; i++) {
+    inyectHistoria()
+    /* MODAL VER MAS */
+    function mostrarVerMas(i) {
 
+        const btn1 = document.querySelector('.btn1');
+        btn1.innerText = "VER RECOMENDACIONES"; /* BOTONES DENTRO DEL MODAL */
 
+        const btn3 = document.querySelector('.btn3');
+        btn3.innerText = "BOTON 3";
 
-function edicionObs (){
+        const btn2 = document.querySelector('.btn2');
+        btn2.innerText = "VER OBSERVACIONES";
+
+        const btn4 = document.querySelector('.btn4');
+        btn4.innerText = "BOTON 4";
+
+        /* evento click para las funciones de editar */
+
+        const parrafoObservaciones = document.querySelector('.pObservaciones');
+        parrafoObservaciones.innerHTML = `
+${datos.observaciones[i]}
+<br>
+${datos.fecha[i]}
+`
+        const parrafoRecomendaciones = document.querySelector('.pRecomendaciones');
+        parrafoRecomendaciones.innerHTML = `
+${datos.recomendaciones[i]}
+<br>
+${datos.fecha[i]}
+`
+        btn3.addEventListener('click', mostrarEditar(i));
+        btn4.addEventListener('click', mostrarEditar(i));
+    }
+
+    function mostrarEditar(i) {
+
+        console.log("funcion editar ");
+        const parrafoObservaciones = document.querySelector('.pObservaciones');
+        parrafoObservaciones.innerHTML = `
+<textarea style="width: 100%" id="textAreaObservaciones" oninput='${edicionObs()}'>${datos.observaciones[i]}</textarea>
+<br>
+<p> fecha: ${datos.fecha[i]}</p>
+<br>
+`
+        const parrafoRecomendaciones = document.querySelector('.pRecomendaciones');
+        parrafoRecomendaciones.innerHTML = `
+<textarea style="width: 100%" id="textAreaRecomendaciones" oninput='${edicionReco()}'>${datos.recomendaciones[i]}</textarea>
+<br>
+<p>fecha: ${datos.fecha[i]}</p>
+<br>
+`
+    }
+}
+
+function edicionObs() {
     const btn3 = document.querySelector('.btn3');
-    btn3.addEventListener('click', ()=>{
+    btn3.addEventListener('click', () => {
         console.log("nada");
 
         const editObs = document.querySelector('#textAreaObservaciones')
 
         console.log(editObs.textContent);
 
-        students.observaciones[i] = editObs.textContent
+        datos.observaciones[i] = editObs.textContent
 
-        console.log(students.observaciones[i]);
-    });    
+        console.log(datos.observaciones[i]);
+    });
 }
 
-function edicionReco (){
+function edicionReco() {
     const btn4 = document.querySelector('.btn4');
-    btn4.addEventListener('click', ()=>{
+    btn4.addEventListener('click', () => {
         console.log("nada");
 
         const editReco = document.querySelector('#textAreaRecomendaciones')
 
-        console.log("no",editReco.textContent);
+        console.log("no", editReco.textContent);
 
-        students.recomendaciones[i] = editReco.textContent
+        datos.recomendaciones[i] = editReco.textContent
 
-        console.log(students.recomendaciones[i]);
-    });    
+        console.log(datos.recomendaciones[i]);
+    });
 }
