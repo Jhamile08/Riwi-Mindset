c645d224e2c1d3e4eea72d0a6a81375d4d2968fe
Daniel <tradedani@gmail.com>
2024-02-18 23:35:38 -0500

    arreglo home teachers

--- RiwiMindset/RiwiMindset-Proceso/Teachers/HomeTeachers/scriptTeachers.js
+++ RiwiMindset/RiwiMindset-Proceso/Teachers/HomeTeachers/scriptTeachers.js
@@ -1,24 +1,5 @@
 import { deleteEventFromServer } from "../SheduleTeachers/TeachersSchedule.js";
 
-document.getElementById("input").addEventListener("change", () => {
-  if (document.body.className.indexOf("dark") === -1) {
-    document.body.classList.add("dark");
-  } else {
-    document.body.classList.remove("dark");
-  }
-});
-
-const navbar = document.querySelector(".navbar");
-
-document.getElementById("input").addEventListener("change", () => {
-  if (navbar.className.indexOf("dark") === -1) {
-    navbar.classList.add("dark");
-  } else {
-    navbar.classList.remove("dark");
-  }
-});
-
-
 /* Inyectar citas de la base de datos json */
 getDataJsonArray();
 
