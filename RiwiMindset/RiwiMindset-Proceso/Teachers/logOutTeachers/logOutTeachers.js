00a22eb571e1fda06912dd961e0371afe43b5891
Daniel <tradedani@gmail.com>
2024-02-18 23:52:02 -0500

    arreglo en remisiones y boton logOut Teachers

--- RiwiMindset/RiwiMindset-Proceso/Teachers/logOutTeachers/logOutTeachers.js
+++ RiwiMindset/RiwiMindset-Proceso/Teachers/logOutTeachers/logOutTeachers.js
@@ -2,5 +2,5 @@ const btnButtonLogOut = document.getElementById('btnLogOut');
 
 btnButtonLogOut.addEventListener('click', ()=>{
     localStorage.removeItem("teacher");
-    window.location.href("../General/indexLogin.html")
-})
+    window.location.href= "../../General/indexLogin.html"
+});
