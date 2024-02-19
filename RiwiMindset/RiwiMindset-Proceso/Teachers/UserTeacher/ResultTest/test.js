a8758991cd4a743d86e0aff351acd4c49d28d694
jsanabrh <juanhoyos0422@gmail.com>
2024-02-19 07:19:30 -0500

    cambio mal

--- RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/ResultTest/test.js
+++ RiwiMindset/RiwiMindset-Proceso/Teachers/UserTeacher/ResultTest/test.js
@@ -32,7 +32,7 @@ let tabla = document.querySelector('.slide tbody')
 tabla.innerHTML += `
     <tr>
     <td>1</td>
-    <td>Logieeeeeeeeeeeeca</td>
+    <td>18/02/2024</td>
     <td>10%</td>
     </tr>
 `
